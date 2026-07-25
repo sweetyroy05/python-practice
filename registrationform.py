b67f37426f67b64e651e5ef64a3ed55890a612c5
Sweety Roy <sweetyroyy05@gmail.com>
2026-07-26 00:49:07 +0530

    Delete registrationform.py

--- registrationform.py
+++
@@ -1,9 +0,0 @@
-print("REGISTRATION FORM")
-#input from user
-name=input("What is Your Name? ")
-age=input("What is Your Age?")
-city=input("Where do You Live? ")
-email=input("Your email? ")
-phone=input("Your phone number? ")
-print("user details are: ")
-print(name, age, city, email, phone, sep=", ", end=".")
